57571558eed0c303dbcfafbf884a940f02b2ea29
JuanGalofre <108921687+JuanGalofre@users.noreply.github.com>
2023-07-26 19:59:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,53 @@
 import streamlit as st
 import pandas as pd 
+def predict_diabetes(form_data):
+    # Load the trained model
+    model = load_model('./diabetes_model.h5')
 
+    mapping = {
+        'Age': {"18 a 24 años":1,"25 a 29 años":2,"30 a 34 años":3,'35 a 39 años':4, '40 a 44 años':5,'45 a 49 años':6,'50 a 54 años':7,'55 a 59 años':8,'60 a 64 años':9,'65 a 69 años':10,'70 a 74 años':11,'75 a 79 años':12,'80 años o más':13},
+        'Education': {'Preescolar':1, 'Escuela primaria':2, 'Escuela secundaria (no graduado)':3,'Escuela secundaria (graduado)':4, 'Universidad o escuela técnica (no graduado)':5, 'Graduado universitario o más':6},
+        'Income': {'Menos de $10,000':1,  '$10,000 a $16,000':2,  '$17,000 a $22,000':3,  '$23,000 a $28,000':4,  '$29,000 a $35,000':5,  '$36,000 a $48,000':6, '$49,000 a $61,000':7, '$62,000 a $75,000':8},
+        'Sex': {'Mujer': 0, 'Hombre': 1},
+        'NoDocbcCost': {False: 0, True: 1},
+        'AnyHealthcare': {False: 0, True: 1},
+        'CholCheck': {False: 0, True: 1},
+        'Smoker': {False: 0, True: 1},
+        'Stroke': {False: 0, True: 1},
+        'PhysActivity': {False: 0, True: 1},
+        'Fruits': {False: 0, True: 1},
+        'Veggies': {False: 0, True: 1},
+        'HvyAlcoholConsump': {False: 0, True: 1},
+        'HighBP': {False: 0, True: 1},
+        'HighChol': {False: 0, True: 1},
+        'BMI': {25.0: 0},
+        'HeartDiseaseorAttack': {False: 0, True: 1}
+    }
+
+    # Convertir los datos del formulario en un DataFrame
+    form_df = pd.DataFrame(form_data)
+
+    # Codificar las características categóricas del formulario
+    def map_values(column_name, value):
+        return mapping[column_name][value] if value in mapping[column_name] else value
+
+    form_encoded = form_df.apply(lambda column: column.apply(lambda x: map_values(column.name, x)))
+
+
+
+    # Escalar las características del formulario usando el mismo scaler que se usó en el entrenamiento
+    scaler = StandardScaler()
+    form_scaled = scaler.fit_transform(form_encoded)
+
+    # Realizar la predicción usando el modelo entrenado
+    predictions = model.predict(form_scaled)
+
+    # Table of probabilities
+    probabilities = pd.DataFrame(predictions, columns=['No diabetes', 'Prediabetes', 'Diabetes'])
+
+    # Get the state
+    state = probabilities.idxmax(axis=1).values[0]
+    return probabilities, state
 
 
 def main():
@@ -33,7 +80,26 @@ def main():
 
     
     if submit_button:
-        st.write("Tu rango de edad seleccionado es:", colesterol)
+        form_data={
+        'Age': [form.edad.data],
+        'Education': [form.nivelEducativo.data],
+        'Income': [form.ingresos.data],
+        'Sex': [form.genero.data],
+        'NoDocbcCost': [form.costoCobertura.data],
+        'AnyHealthcare': [form.coberturaMedica.data],
+        'CholCheck': [form.colesterolCheck.data],
+        'Smoker': [form.fumador.data],
+        'Stroke': [form.acv.data],
+        'PhysActivity': [form.actividadFisica.data],
+        'Fruits': [form.frutas.data],
+        'Veggies': [form.verduras.data],
+        'HvyAlcoholConsump': [form.alcohol.data],
+        'HighBP': [form.tension.data],
+        'HighChol': [form.colesterol.data],
+        'BMI': [form.bmi.data],
+        'HeartDiseaseorAttack': [form.enfermedadesCardiacas.data]}
+         result, state = predict_diabetes(form_data)
+        st.write(result)
 
 
 if __name__ == "__main__":
