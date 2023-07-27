2bb5c59bea1e91ba33f91937b61361ffdfcb6c12
JuanGalofre <108921687+JuanGalofre@users.noreply.github.com>
2023-07-26 19:25:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import pandas as pd 
 def main():
-    edades=["18 a 24 años"]
-    ages=["18 to 24"]
+    edades=["18 a 24 años","25 a 29 años"]
+    ages=["18 to 24","25 to 29"]
     st.title("Predictor de diabetes")
     with st.form(key="form1"):
-        firstname=st.selectbox("Selecciona tu rango de edad",edades,kwargs=ages)
-        lastname=st.text_input("Lastname")
-        dubidubiduba=st.date_input("Dubidubidubiwa")
-
+        edad=st.selectbox("Selecciona tu rango de edad",edades,kwargs=ages)
         submit_button=st.form_submit_button()
+    if submit_button:
+        st.write("Tu rango de edad seleccionado es:", edad)
 if __name__ == "__main__":
     main()
